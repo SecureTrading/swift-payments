818648202d04cc7ca7f64f794f2a671ae0661bde
Tomasz Iwaszek <tiwaszek@pgs-soft.com>
2020-05-06 11:53:52 +0200

    check imports in unittests

--- Example/ExampleTests/ExampleTests.swift
+++ Example/ExampleTests/ExampleTests.swift
@@ -7,12 +7,19 @@
 //
 
 import XCTest
+import SecureTradingUI
+import SecureTradingCore
 @testable import Example
 
 class ExampleTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        // Test Core framework availability
+        let apiClient = DefaultAPIClient()
+        // Test UI framework availability
+        let cardVC = CardViewController(view: CardView(), viewModel: CardViewModel())
     }
 
     override func tearDownWithError() throws {
