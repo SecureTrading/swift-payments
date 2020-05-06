993a5797e25913acb90713fff72493223b9df221
Tomasz Iwaszek <tiwaszek@pgs-soft.com>
2020-05-06 11:13:42 +0200

    prepare some base classes

---
+++ SecureTrading/SecureTradingUI/Classes/Modules/Cards/CardView.swift
@@ -0,0 +1,12 @@
+//
+//  CardView.swift
+//  SecureTradingUI
+//
+//  Created by TIWASZEK on 06/05/2020.
+//  Copyright © 2020 TIWASZEK. All rights reserved.
+//
+
+import UIKit
+
+public final class CardView: UIView {
+}
---
+++ SecureTrading/SecureTradingUI/Classes/Modules/Cards/CardViewController.swift
@@ -0,0 +1,13 @@
+//
+//  CardViewController.swift
+//  SecureTradingUI
+//
+//  Created by TIWASZEK on 06/05/2020.
+//  Copyright © 2020 TIWASZEK. All rights reserved.
+//
+
+import Foundation
+
+public final class CardViewController: BaseViewController<CardView, CardViewModel> {
+
+}
