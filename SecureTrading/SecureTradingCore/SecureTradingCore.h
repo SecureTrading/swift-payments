664191e162d071e08802b166ec40cd086924b784
Tomasz Iwaszek <tiwaszek@pgs-soft.com>
2020-05-06 10:05:05 +0200

    add core framework

---
+++ SecureTrading/SecureTradingCore/SecureTradingCore.h
@@ -0,0 +1,19 @@
+//
+//  SecureTrading.h
+//  SecureTrading
+//
+//  Created by TIWASZEK on 06/05/2020.
+//  Copyright © 2020 TIWASZEK. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SecureTrading.
+FOUNDATION_EXPORT double SecureTradingVersionNumber;
+
+//! Project version string for SecureTrading.
+FOUNDATION_EXPORT const unsigned char SecureTradingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SecureTrading/PublicHeader.h>
+
+
