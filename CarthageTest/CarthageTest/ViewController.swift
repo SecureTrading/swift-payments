3f21c45a50a5c61be44565b6094a651d90f55a68
Tomasz Iwaszek <tiwaszek@pgs-soft.com>
2020-05-08 10:51:48 +0200

    add carthage test example

---
+++ CarthageTest/CarthageTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CarthageTest
+//
+//  Created by TIWASZEK on 08/05/2020.
+//  Copyright © 2020 TIWASZEK. All rights reserved.
+//
+
+import UIKit
+import SecureTradingUI
+import SecureTradingCore
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Test Core framework availability
+        let apiClient = DefaultAPIClient()
+        // Test UI framework availability
+        let cardVC = CardViewController(view: CardView(), viewModel: CardViewModel())
+    }
+
+
+}
+
