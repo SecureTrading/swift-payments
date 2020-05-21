9066743bec8c4f01b684cfad2c9744c96a31f27b
Maciej Chrzastek <mchrzastek@pgs-soft.com>
2020-05-21 16:42:52 +0200

    SecureTradingCard framework init

---
+++ SecureTrading/SecureTradingCard/SecureTradingCard.h
@@ -0,0 +1,19 @@
+//
+//  SecureTradingCard.h
+//  SecureTradingCard
+//
+//  Created by MCHRZASTEK on 21/05/2020.
+//  Copyright © 2020 TIWASZEK. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SecureTradingCard.
+FOUNDATION_EXPORT double SecureTradingCardVersionNumber;
+
+//! Project version string for SecureTradingCard.
+FOUNDATION_EXPORT const unsigned char SecureTradingCardVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SecureTradingCard/PublicHeader.h>
+
+
