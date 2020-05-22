64becd47efd4d910eeb16f601189c6439f97a97a
Maciej Chrzastek <mchrzastek@pgs-soft.com>
2020-05-22 15:40:19 +0200

    Test card types

--- SecureTrading/SecureTradingCardTests/TestCardType.swift
+++ SecureTrading/SecureTradingCardTests/TestCardType.swift
@@ -9,25 +9,70 @@ import XCTest
 @testable import SecureTradingCard
 
 class TestCardType: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
+    
     func test_isVisa() {
-        let visaCards = KnownCards.visaCards
+        let expectedType = CardType.visa
+        let cardNumbers = KnownCards.visaCards
         
-        for card in visaCards {
-            XCTAssertEqual(CardType.visa, CardValidator.cardType(for: card))
+        for card in cardNumbers {
+            XCTAssertEqual(expectedType, CardValidator.cardType(for: card))
+        }
+    }
+    func test_isMasterCard() {
+        let expectedType = CardType.mastercard
+        let cardNumbers = KnownCards.mastercardCards
+        
+        for card in cardNumbers {
+            XCTAssertEqual(expectedType, CardValidator.cardType(for: card))
+        }
+    }
+    func test_isMaestro() {
+        let expectedType = CardType.maestro
+        let cardNumbers = KnownCards.maestroCards
+        
+        for card in cardNumbers {
+            XCTAssertEqual(expectedType, CardValidator.cardType(for: card))
+        }
+    }
+    func test_isAmex() {
+        let expectedType = CardType.amex
+        let cardNumbers = KnownCards.amexCards
+        
+        for card in cardNumbers {
+            XCTAssertEqual(expectedType, CardValidator.cardType(for: card))
+        }
+    }
+    func test_isJCB() {
+        let expectedType = CardType.jcb
+        let cardNumbers = KnownCards.jcbCards
+        
+        for card in cardNumbers {
+            XCTAssertEqual(expectedType, CardValidator.cardType(for: card))
+        }
+    }
+    func test_isDiners() {
+        let expectedType = CardType.diners
+        let cardNumbers = KnownCards.dinerCards
+        
+        for card in cardNumbers {
+            XCTAssertEqual(expectedType, CardValidator.cardType(for: card))
+        }
+    }
+    func test_isDiscover() {
+        let expectedType = CardType.discover
+        let cardNumbers = KnownCards.discoverCards
+        
+        for card in cardNumbers {
+            print("Discover: \(card)")
+            XCTAssertEqual(expectedType, CardValidator.cardType(for: card))
+        }
+    }
+    func test_isPIBA() {
+        let expectedType = CardType.piba
+        let cardNumbers = KnownCards.pibaCards
+        
+        for card in cardNumbers {
+            XCTAssertEqual(expectedType, CardValidator.cardType(for: card))
         }
     }
     func testPerformanceExample() throws {
@@ -36,5 +81,5 @@ class TestCardType: XCTestCase {
             // Put the code you want to measure the time of here.
         }
     }
-
+    
 }
