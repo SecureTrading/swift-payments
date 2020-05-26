bf1b78406b4cbf3606cd12e979edaec0e2e5d742
Tomasz Iwaszek <tiwaszek@pgs-soft.com>
2020-05-26 11:03:11 +0200

    card number input view improvements

--- SecureTrading/SecureTradingUI/Source Files/Common/Views/Form/CardNumberInputView.swift
+++ SecureTrading/SecureTradingUI/Source Files/Common/Views/Form/CardNumberInputView.swift
@@ -2,35 +2,43 @@
 //  CardNumberInputVIew.swift
 //  SecureTradingUI
 //
-//  Created by TIWASZEK on 25/05/2020.
-//  Copyright © 2020 TIWASZEK. All rights reserved.
-//
 
 import UIKit
 
 @objc public final class CardNumberInputView: SecureFormInputView {
-    /**
-     The string that is used to separate different groups in a card number.
-     */
-    public var cardNumberSeparator: String = "-"
+
+    // MARK: Public Properties
+
+    public var cardNumberSeparator: String = .space
+
+    public var cardNumber: CardNumber {
+        let textFieldTextWithoutSeparators = cardNumberFormat.removeSeparator(cardNumber: text ?? .empty)
+        return CardNumber(rawValue: textFieldTextWithoutSeparators)
+    }
+
+    public var cardTypeContainer: CardTypeContainer = CardTypeContainer()
+
+    // MARK: Private Properties
+
+    private var cardNumberFormat: CardNumberFormat {
+        return CardNumberFormat(cardTypeContainer: cardTypeContainer, separator: cardNumberSeparator)
+    }
 }
 
 // MARK: TextField delegate
 
 extension CardNumberInputView {
-
     public func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-        // Current text in text field, formatted and unformatted:
-              let textFieldTextFormatted = NSString(string: textField.text ?? "")
-              // Text in text field after applying changes, formatted and unformatted:
-              let newTextFormatted = textFieldTextFormatted.replacingCharacters(in: range, with: string)
-        
+        let textFieldTextWithSeparators = NSString(string: textField.text ?? .empty)
+        let newTextWithSeparators = textFieldTextWithSeparators.replacingCharacters(in: range, with: string)
+        let newTextWithoutSeparators = cardNumberFormat.removeSeparator(cardNumber: newTextWithSeparators)
+
+        if !newTextWithoutSeparators.isEmpty, !newTextWithoutSeparators.isNumeric() {
+            return false
+        }
+
+        cardNumberFormat.addSeparators(range: range, inTextField: textField, replaceWith: string)
 
         return false
     }
-
-//    public override func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-//        textField.resignFirstResponder()
-//        return true
-//    }
 }
