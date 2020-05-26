521c7f01734eb6e6f1fce548d6e5d22b8fdb03eb
Maciej Chrzastek <mchrzastek@pgs-soft.com>
2020-05-26 15:57:42 +0200

    Move date extension to separate file

--- SecureTrading/SecureTradingCard/Source files/CardValidator.swift
+++ SecureTrading/SecureTradingCard/Source files/CardValidator.swift
@@ -80,14 +80,3 @@ public class CardValidator {
         return !expirationDate.isEarlierThanCurrentMonth()
     }
 }
-
-fileprivate extension Date {
-    /// Checks if date is earlier than current date, compares only month and year components
-    /// - Returns: if date is in past
-    func isEarlierThanCurrentMonth() -> Bool {
-        let now = Date()
-        let currentComponents = Calendar.current.dateComponents([.year, .month], from: now)
-        guard let nowFromComponents = Calendar.current.date(from: currentComponents) else { return false }
-        return self < nowFromComponents
-    }
-}
