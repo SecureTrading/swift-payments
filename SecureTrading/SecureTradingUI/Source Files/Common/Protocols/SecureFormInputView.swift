067026b51c0de976d5b90e3dc8a7c368bbbd6547
Tomasz Iwaszek <tiwaszek@pgs-soft.com>
2020-06-04 12:02:51 +0200

    build input style manager #3

--- SecureTrading/SecureTradingUI/Source Files/Common/Protocols/SecureFormInputView.swift
+++ SecureTrading/SecureTradingUI/Source Files/Common/Protocols/SecureFormInputView.swift
@@ -60,14 +60,52 @@ import UIKit
     // MARK: - images
 
     var textFieldImage: UIImage? { get set }
-
-    //func customizeView(inputViewStyleManager: InputViewStyleManager)
 }
 
 extension SecureFormInputView {
     public func customizeView(inputViewStyleManager: InputViewStyleManager?) {
-        if let titleColor = inputViewStyleManager?.textColor {
+        if let titleColor = inputViewStyleManager?.titleColor {
             self.titleColor = titleColor
+        }
+
+        if let textFieldBorderColor = inputViewStyleManager?.textFieldBorderColor {
+            self.textFieldBorderColor = textFieldBorderColor
+        }
+
+        if let textFieldBackgroundColor = inputViewStyleManager?.textFieldBackgroundColor {
+            self.textFieldBackgroundColor = textFieldBackgroundColor
+        }
+
+        if let textColor = inputViewStyleManager?.textColor {
+            self.textColor = textColor
+        }
+
+        if let placeholderColor = inputViewStyleManager?.placeholderColor {
+            self.placeholderColor = placeholderColor
+        }
+
+        if let errorColor = inputViewStyleManager?.errorColor {
+            self.errorColor = errorColor
+        }
+
+        if let titleFont = inputViewStyleManager?.titleFont {
+            self.titleFont = titleFont
+        }
+
+        if let textFont = inputViewStyleManager?.textFont {
+            self.textFont = textFont
+        }
+
+        if let placeholderFont = inputViewStyleManager?.placeholderFont {
+            self.placeholderFont = placeholderFont
+        }
+
+        if let errorFont = inputViewStyleManager?.errorFont {
+            self.errorFont = errorFont
+        }
+
+        if let textFieldImage = inputViewStyleManager?.textFieldImage {
+            self.textFieldImage = textFieldImage
         }
     }
 }
