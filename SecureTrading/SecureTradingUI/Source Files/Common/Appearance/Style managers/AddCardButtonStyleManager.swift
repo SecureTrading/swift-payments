99a297fcec9d739faadaccd088bc77deee7cf6c7
Maciej Chrzastek <mchrzastek@pgs-soft.com>
2020-06-15 17:57:58 +0200

    Add card button - default manager

--- SecureTrading/SecureTradingUI/Source Files/Common/Appearance/Style managers/AddCardButtonStyleManager.swift
+++ SecureTrading/SecureTradingUI/Source Files/Common/Appearance/Style managers/AddCardButtonStyleManager.swift
@@ -3,6 +3,19 @@
 //  SecureTradingUI
 //
 
-import Foundation
+import UIKit
 
-@objc public class AddCardButtonStyleManager: RequestButtonStyleManager { }
+@objc public class AddCardButtonStyleManager: RequestButtonStyleManager {
+    @objc public static func `default`() -> AddCardButtonStyleManager {
+        return AddCardButtonStyleManager(titleColor: .white,
+                                         enabledBackgroundColor: .black,
+                                         disabledBackgroundColor: UIColor.lightGray.withAlphaComponent(0.6),
+                                         borderColor: .clear,
+                                         titleFont: UIFont.systemFont(ofSize: 16, weight: .medium),
+                                         spinnerStyle: .white,
+                                         spinnerColor: .white,
+                                         buttonContentHeightMargins: HeightMargins(top: 15, bottom: 15),
+                                         borderWidth: 0,
+                                         cornerRadius: 6)
+    }
+}
