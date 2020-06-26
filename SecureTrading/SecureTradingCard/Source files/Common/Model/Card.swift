14092a417a94fd9bf584b9acd6514d3af073b733
Tomasz Iwaszek <tiwaszek@pgs-soft.com>
2020-06-26 12:57:07 +0200

    card improvements

--- SecureTrading/SecureTradingCard/Source files/Common/Model/Card.swift
+++ SecureTrading/SecureTradingCard/Source files/Common/Model/Card.swift
@@ -6,20 +6,22 @@
 import Foundation
 
 @objc public class Card: NSObject {
-
     @objc public let cardNumber: CardNumber?
 
     @objc public let securityCode: CVC?
 
-    @objc public var cardType: CardType {
-        return CardValidator.cardType(for: cardNumber?.rawValue ?? "", cardTypes: CardType.allCases)
-    }
-
     @objc public let expiryDate: ExpiryDate?
 
-    @objc public init(cardNumber: CardNumber?, securityCode: CVC?, expiryDate: ExpiryDate?) {
+    @objc public var cardTypeContainer: CardTypeContainer
+
+    @objc public var cardType: CardType {
+        return CardValidator.cardType(for: cardNumber?.rawValue ?? .empty, cardTypes: cardTypeContainer.cardTypes)
+    }
+
+    @objc public init(cardNumber: CardNumber?, securityCode: CVC?, expiryDate: ExpiryDate?, cardTypeContainer: CardTypeContainer = CardTypeContainer(cardTypes: CardType.allCases)) {
         self.cardNumber = cardNumber
         self.securityCode = securityCode
         self.expiryDate = expiryDate
+        self.cardTypeContainer = cardTypeContainer
     }
 }
