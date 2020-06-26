5ba8a733fe9aed86900912475fa8ac7f466f9d6b
Tomasz Iwaszek <tiwaszek@pgs-soft.com>
2020-06-26 10:37:42 +0200

    card reference - parsing improvement

--- SecureTrading/SecureTradingCore/Source files/API/Models/STCardReference.swift
+++ SecureTrading/SecureTradingCore/Source files/API/Models/STCardReference.swift
@@ -8,7 +8,7 @@ import Foundation
 /// Represents a transaction and card details used for that transaction
 /// Can be used for future payments without the need to provide all card details
 @objc public class STCardReference: NSObject {
-    public let transactionReference: String
+    public let transactionReference: String?
     public let cardType: String
     public let maskedPan: String
 
@@ -17,7 +17,7 @@ import Foundation
     ///   - reference: transaction reference, returned on ACCOUNTCHECK
     ///   - cardType: String value representing card type, e.g VISA
     ///   - pan: masked pan number returned in response
-    @objc public init(reference: String, cardType: String, pan: String) {
+    @objc public init(reference: String?, cardType: String, pan: String) {
         self.transactionReference = reference
         self.cardType = cardType
         self.maskedPan = pan
