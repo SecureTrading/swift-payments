7eafe6bbf1320b559e7d8547db17c566ccaf9baa
Maciej Chrzastek <mchrzastek@pgs-soft.com>
2020-06-29 11:17:29 +0200

    Add loader view to Example app

--- Example/Example/Source Files/Common/Views/LoaderView.swift
+++ Example/Example/Source Files/Common/Views/LoaderView.swift
@@ -5,32 +5,75 @@
 
 import UIKit
 
-// TODO: improve
-class LoaderView: UIView {
-    private var spinner: UIActivityIndicatorView?
+/// A Simple loader view that consists of a grayed out square at the center of the screen with white UIActivity Indicator
+class LoaderView: BaseView {
 
-    init() {
-        super.init(frame: .zero)
-        self.backgroundColor = UIColor.gray.withAlphaComponent(0.7)
-        self.spinner = UIActivityIndicatorView(style: .whiteLarge)
-        self.spinner?.hidesWhenStopped = true
-        self.addSubview(self.spinner!)
-        self.highlightIfNeeded()
-    }
+    // the actual spinner
+    private var spinner = UIActivityIndicatorView(style: .whiteLarge)
 
-    /// - SeeAlso: NSCoding.init?(coder:)
-    required init?(coder _: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
+    // center square container view
+    private let spinnerContainerView = UIView(frame: .zero)
+
+    // optional label at the bottom of container view
+    private let label = UILabel()
 
     func start() {
-        self.spinner?.addConstraints([
-                         equal(self, \.centerYAnchor, \.centerYAnchor, constant: 0),
-                         equal(self, \.centerXAnchor, \.centerXAnchor, constant: 0)
-                     ])
-        self.spinner?.startAnimating()
+        spinner.startAnimating()
     }
     func stop() {
-        self.spinner?.stopAnimating()
+        spinner.stopAnimating()
+    }
+}
+
+extension LoaderView: ViewSetupable {
+    /// - SeeAlso: ViewSetupable.setupProperties
+    func setupProperties() {
+        // Spinner container
+        spinnerContainerView.translatesAutoresizingMaskIntoConstraints = false
+        spinnerContainerView.backgroundColor = UIColor.black.withAlphaComponent(0.5)
+        spinnerContainerView.layer.cornerRadius = 12
+        spinnerContainerView.clipsToBounds = true
+
+        // spinner
+        spinner.hidesWhenStopped = true
+
+        // label
+        label.text = LocalizableKeys.Alerts.processing.localizedStringOrEmpty
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = UIColor.white
+        label.font = UIFont.systemFont(ofSize: 24)
+        label.textAlignment = .center
+    }
+
+    /// - SeeAlso: ViewSetupable.setupViewHierarchy
+    func setupViewHierarchy() {
+        addSubview(spinnerContainerView)
+        addSubview(spinner)
+        addSubview(label)
+    }
+
+    /// - SeeAlso: ViewSetupable.setupConstraints
+    func setupConstraints() {
+        // spinner container
+        spinnerContainerView.addConstraints([
+            equal(\.heightAnchor, greaterOrEqual: 150),
+            equal(\.widthAnchor, greaterOrEqual: 150),
+            equal(self, \.centerYAnchor, \.centerYAnchor, constant: 0),
+            equal(self, \.centerXAnchor, \.centerXAnchor, constant: 0)
+        ])
+
+        // spinner
+        spinner.addConstraints([
+            equal(self, \.centerYAnchor, \.centerYAnchor, constant: 0),
+            equal(self, \.centerXAnchor, \.centerXAnchor, constant: 0)
+        ])
+
+        // label
+        label.addConstraints([
+            equal(spinnerContainerView, \.bottomAnchor, \.bottomAnchor, constant: -10),
+            equal(spinnerContainerView, \.centerXAnchor, \.centerXAnchor, constant: 0),
+            equal(spinnerContainerView, \.leadingAnchor, \.leadingAnchor, constant: 5),
+            equal(spinnerContainerView, \.trailingAnchor, \.trailingAnchor, constant: -5)
+        ])
     }
 }
