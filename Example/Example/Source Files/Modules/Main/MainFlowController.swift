388dc5556a17de6de12aaa61509973d8764712a0
Maciej Chrzastek <mchrzastek@pgs-soft.com>
2020-06-30 17:12:31 +0200

    Add new parameter for visible fields in DropIn

--- Example/Example/Source Files/Modules/Main/MainFlowController.swift
+++ Example/Example/Source Files/Modules/Main/MainFlowController.swift
@@ -43,7 +43,8 @@ final class MainFlowController: BaseNavigationFlowController {
                     MainViewModel.Row.performAccountCheck,
                     MainViewModel.Row.performAccountCheckWithAuth,
                     MainViewModel.Row.subscriptionOnSTEngine,
-                    MainViewModel.Row.subscriptionOnMerchantEngine
+                    MainViewModel.Row.subscriptionOnMerchantEngine,
+                    MainViewModel.Row.payFillCVV
                 ]),
             MainViewModel.Section.onMerchant(rows:
                 [
@@ -74,6 +75,8 @@ final class MainFlowController: BaseNavigationFlowController {
                 self.showWalletView()
             case .didTapShowDropInControllerWithCustomView(let jwt):
                 self.showDropInViewController(jwt: jwt, handleCardinalWarnings: false, addCustomView: true)
+            case .didTapShowDropInControllerSaveCardFillCVV(let jwt):
+                self.showDropInViewController(jwt: jwt, handleCardinalWarnings: false, addCustomView: true, typeDescriptions: [.auth], visibleFields: [.securityCode3])
             }
         }
         return mainViewController
@@ -87,13 +90,13 @@ final class MainFlowController: BaseNavigationFlowController {
         push(vc, animated: true)
     }
 
-    func showDropInViewController(jwt: String, handleCardinalWarnings: Bool, addCustomView: Bool, typeDescriptions: [TypeDescription] = [.threeDQuery, .auth]) {
+    func showDropInViewController(jwt: String, handleCardinalWarnings: Bool, addCustomView: Bool, typeDescriptions: [TypeDescription] = [.threeDQuery, .auth], visibleFields: [DropInViewVisibleFields] = DropInViewVisibleFields.allCases) {
         // swiftlint:disable line_length
         let inputViewStyleManager = InputViewStyleManager(titleColor: UIColor.gray, textFieldBorderColor: UIColor.black.withAlphaComponent(0.8), textFieldBackgroundColor: .clear, textColor: .black, placeholderColor: UIColor.lightGray.withAlphaComponent(0.8), errorColor: UIColor.red.withAlphaComponent(0.8), titleFont: UIFont.systemFont(ofSize: 16, weight: .regular), textFont: UIFont.systemFont(ofSize: 16, weight: .regular), placeholderFont: UIFont.systemFont(ofSize: 16, weight: .regular), errorFont: UIFont.systemFont(ofSize: 12, weight: .regular), textFieldImage: nil, titleSpacing: 5, errorSpacing: 3, textFieldHeightMargins: HeightMargins(top: 10, bottom: 10), textFieldBorderWidth: 1, textFieldCornerRadius: 6)
 
         let payButtonStyleManager = PayButtonStyleManager(titleColor: .white, enabledBackgroundColor: .black, disabledBackgroundColor: UIColor.lightGray.withAlphaComponent(0.6), borderColor: .clear, titleFont: UIFont.systemFont(ofSize: 16, weight: .medium), spinnerStyle: .white, spinnerColor: .white, buttonContentHeightMargins: HeightMargins(top: 15, bottom: 15), borderWidth: 0, cornerRadius: 6)
 
-        let dropInViewStyleManager = DropInViewStyleManager(inputViewStyleManager: inputViewStyleManager, visibleFields: [DropInViewVisibleFields.securityCode4], requestButtonStyleManager: payButtonStyleManager, backgroundColor: .white, spacingBeetwenInputViews: 25, insets: UIEdgeInsets(top: 25, left: 35, bottom: -30, right: -35))
+        let dropInViewStyleManager = DropInViewStyleManager(inputViewStyleManager: inputViewStyleManager, visibleFields: visibleFields, requestButtonStyleManager: payButtonStyleManager, backgroundColor: .white, spacingBeetwenInputViews: 25, insets: UIEdgeInsets(top: 25, left: 35, bottom: -30, right: -35))
 
         let toolbarStyleManager = CardinalToolbarStyleManager(textColor: .black, textFont: UIFont.systemFont(ofSize: 16, weight: .bold), backgroundColor: .yellow, headerText: "Trust payment checkout", buttonText: "WHAAT")
         let labelStyleManager = CardinalLabelStyleManager(textColor: .brown, textFont: UIFont.systemFont(ofSize: 13, weight: .heavy), headingTextColor: .green, headingTextFont: UIFont.italicSystemFont(ofSize: 15))
@@ -112,9 +115,9 @@ final class MainFlowController: BaseNavigationFlowController {
         let isDeferInit = addCustomView
 
         let dropInVC = ViewControllerFactory.shared.dropInViewController(jwt: jwt, typeDescriptions: typeDescriptions, gatewayType: .eu, username: appFoundation.keys.merchantUsername, isLiveStatus: false, isDeferInit: isDeferInit, customDropInView: customDropInView, dropInViewStyleManager: dropInViewStyleManager, cardTypeToBypass: [], cardinalStyleManager: cardinalStyleManager, payButtonTappedClosureBeforeTransaction: { [unowned self] controller in
-            guard let customDropInView = customDropInView else { return }
             // updates JWT with credentialsonfile flag
-            guard let updatedJWT = self.mainViewModel?.getJwtTokenWithoutCardData(storeCard: customDropInView.isSaveCardSelected) else { return }
+            let storeCard = customDropInView?.isSaveCardSelected ?? false
+            guard let updatedJWT = self.mainViewModel?.getJwtTokenWithoutCardData(storeCard: storeCard, parentTransactionReference: jwt.parentReference) else { return }
             // update vc with new jwt
             controller.updateJWT(newValue: updatedJWT)
         }, successfulPaymentCompletion: { [unowned self] _, successMessage, cardReference in
