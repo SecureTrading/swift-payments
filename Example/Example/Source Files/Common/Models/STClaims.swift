5e4d5ea5cffafdf9107ee5c63d10eaede9a62394
Maciej Chrzastek <mchrzastek@pgs-soft.com>
2020-06-30 17:09:51 +0200

    Get parent ref from encoded JWT claim

--- Example/Example/Source Files/Common/Models/STClaims.swift
+++ Example/Example/Source Files/Common/Models/STClaims.swift
@@ -57,3 +57,26 @@ struct Payload: Codable {
         self.credentialsonfile = credentialsonfile
     }
 }
+
+extension String {
+    private func base64UrlDecode(_ value: String) -> Data? {
+        var base64 = value
+            .replacingOccurrences(of: "-", with: "+")
+            .replacingOccurrences(of: "_", with: "/")
+        let length = Double(base64.lengthOfBytes(using: String.Encoding.utf8))
+        let requiredLength = 4 * ceil(length / 4.0)
+        let paddingLength = requiredLength - length
+        if paddingLength > 0 {
+            let padding = "".padding(toLength: Int(paddingLength), withPad: "=", startingAt: 0)
+            base64 += padding
+        }
+        return Data(base64Encoded: base64, options: .ignoreUnknownCharacters)
+    }
+
+    var parentReference: String? {
+        let body = self.components(separatedBy: ".")[1]
+        guard let decoded = base64UrlDecode(body) else { return nil }
+        guard let payload = try? JSONDecoder().decode(STClaims.self, from: decoded).payload else { return nil }
+        return payload.parenttransactionreference
+    }
+}
