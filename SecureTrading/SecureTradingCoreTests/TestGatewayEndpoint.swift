843cccb63dd58782778d53976ee896a6f2e712cd
Tomasz Iwaszek <tiwaszek@pgs-soft.com>
2020-07-01 13:07:46 +0200

    add unit test for new domain type

--- SecureTrading/SecureTradingCoreTests/TestGatewayEndpoint.swift
+++ SecureTrading/SecureTradingCoreTests/TestGatewayEndpoint.swift
@@ -13,6 +13,11 @@ class TestGatewayEndpoint: XCTestCase {
         let sut = GatewayType.eu.host
         XCTAssertEqual(expected, sut)
     }
+    func test_gatewayEndpointForEUBackup() {
+        let expected = "webservices2.securetrading.net"
+        let sut = GatewayType.euBackup.host
+        XCTAssertEqual(expected, sut)
+    }
     func test_gatewayEndpointForUS() {
         let expected = "webservices.securetrading.us"
         let sut = GatewayType.us.host
